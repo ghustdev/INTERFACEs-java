6a1e8307014617d47d035cb73687b5f4035f0423
ghustdev <gustavocardoso0019@gmail.com>
2026-02-12 20:38:00 -0300

    Feat: create a project Car Rental with entities and services, but withou Interfaces

---
+++ src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import entities.CarRental;
+import entities.Vehicle;
+import services.BrasilTaxService;
+import services.RentalService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	static void main() {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println("Enter the data: ");
+		System.out.print("Car model: ");
+		String carModel = sc.nextLine();
+		System.out.print("Date of start: ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), dtf);
+		System.out.print("Date of finish: ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), dtf);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		System.out.println("Enter the price per hour: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.println("Enter the price per day: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrasilTaxService());
+		
+		rentalService.processsInvoice(cr);
+		
+		System.out.println("INVOICE: ");
+		System.out.println("Basic payment: " + cr.getInvoice().getBasicPayment());
+		System.out.println("Tax: " + cr.getInvoice().getTex());
+		System.out.println("Total payment: " + cr.getInvoice().getTotalPayment());
+		
+		sc.close();
+	}
+}
